d96155564de7c65f0833d736376dfc8497cd68f5
ben-sentenac <bsr-web-09@proton.me>
2022-10-27 15:18:00 +0200

    modify meta

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <meta name="description" content="Création de site internet en ariège,Developpeur freelance situé 
-      à foix en Ariège spécialisé dans la création et la conception de site internet.
-      Site vitrine, e-commerce, site internet sur mesure, site associatif. Développeur php,laravel,wordpress,javascript,react et nextjs"/>
-      <meta name="keywords" content="création de site web,création de boutique en ligne, 
-      conception de site internet, seo, sites internet, React, nextjs, informatique, php , lararvel, wordpress, site internet wordpress, 
-      site vitrine sur mesure, site internet a Foix, javascript,Ariège, Pyrénées, Pamiers, Foix, occitanie"/>
+      <meta name="description" content="Developpeur freelance situé 
+      à foix en Ariège spécialisé dans la création et la conception de site internet. Site vitrine, e-commerce, site sur-mesure..."/>
       <meta property="og:title" content="Benoit sentenac-réou|création de site internet|foix|06 70 36 92 61"/>
       <meta property="og:type" content="website"/>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
